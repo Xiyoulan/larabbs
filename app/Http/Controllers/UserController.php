fee0cbcf429246cc4af97aff68bbfed805c6982c
Xiyoulan <740518003@qq.com>
2018-07-17 17:00:17 +0800

    basic user profile editing

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 use Illuminate\Http\Request;
 
 class UserController extends Controller {
@@ -10,6 +11,17 @@ class UserController extends Controller {
     //展示个人页面
     public function show(User $user) {
         return view('users.show', compact('user'));
+    }
+
+    //编辑个人资料页面
+    public function edit(User $user) {
+        return view('users.edit', compact('user'));
+    }
+       //编辑个人资料页面
+    public function update(UserRequest $request,User $user) {
+       $user ->update($request->all());
+       return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功!');
+        
     }
 
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,35 +1,35 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'LaraBBS')</title>
+        <title>@yield('title', 'LaraBBS')</title>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
 
-<body>
-    <div id="app" class="{{ route_class() }}-page">
+    <body>
+        <div id="app" class="{{ route_class() }}-page">
 
-        @include('layouts._header')
+            @include('layouts._header')
 
-        <div class="container">
+            <div class="container">
+                @include('layouts._message')
+                @yield('content')
 
-            @yield('content')
+            </div>
 
+            @include('layouts._footer')
         </div>
 
-        @include('layouts._footer')
-    </div>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-</body>
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
 </html>
 
