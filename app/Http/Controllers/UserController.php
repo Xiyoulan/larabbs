d9df87718625c702aaae61d81ec656b8735e080b
Xiyoulan <740518003@qq.com>
2018-07-17 18:54:09 +0800

    only authorized user can edit user profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,9 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller {
 
+    public function __construct() {
+        $this->middleware('auth',['except' => ['show']]);
+    }
     //展示个人页面
     public function show(User $user) {
         return view('users.show', compact('user'));
@@ -16,10 +19,12 @@ class UserController extends Controller {
 
     //编辑个人资料页面
     public function edit(User $user) {
+        $this->authorize('update',$user);
         return view('users.edit', compact('user'));
     }
        //编辑个人资料页面
     public function update(UserRequest $request,ImageUploadHandler $uploader,User $user) {
+       $this->authorize('update',$user);
        $data =$request->all();
        if($request->avatar){
            //上传头像
