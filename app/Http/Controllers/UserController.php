724ffb518f0edf96168649d8cba199b026172403
Xiyoulan <740518003@qq.com>
2018-07-17 18:33:41 +0800

    User can upload avatar

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
 use Illuminate\Http\Request;
@@ -18,8 +19,16 @@ class UserController extends Controller {
         return view('users.edit', compact('user'));
     }
        //编辑个人资料页面
-    public function update(UserRequest $request,User $user) {
-       $user ->update($request->all());
+    public function update(UserRequest $request,ImageUploadHandler $uploader,User $user) {
+       $data =$request->all();
+       if($request->avatar){
+           //上传头像
+           $result = $uploader ->save($request->avatar, 'avatar', $user->id,362);
+           if($result){
+               $data['avatar'] = $result['path']; 
+           }
+       }
+       $user ->update($data);
        return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功!');
         
     }
