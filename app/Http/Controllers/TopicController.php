e76ebba2f7dbb6fadf8e0881159c301ae31ba03e
Xiyoulan <740518003@qq.com>
2018-07-18 18:12:00 +0800

    author can delete topic

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -10,58 +10,67 @@ use App\Handlers\ImageUploadHandler;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 
-class TopicController extends Controller {
+class TopicController extends Controller
+{
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-    public function index(Request $request) {
-        $topics = Topic::with('user','category')->withOrder($request->order)->paginate(20);
+    public function index(Request $request)
+    {
+        $topics = Topic::with('user', 'category')->withOrder($request->order)->paginate(20);
         return view('topics.index', compact('topics'));
     }
 
-    public function show(Topic $topic) {
+    public function show(Topic $topic)
+    {
         return view('topics.show', compact('topic'));
     }
 
-    public function create(Topic $topic) {
+    public function create(Topic $topic)
+    {
         $categories = Category::all();
-        return view('topics.create_and_edit', compact('topic','categories'));
+        return view('topics.create_and_edit', compact('topic', 'categories'));
     }
 
-    public function store(TopicRequest $request,Topic $topic) {
+    public function store(TopicRequest $request, Topic $topic)
+    {
         $topic->fill($request->all());
-        $topic->user_id = Auth::id(); 
+        $topic->user_id = Auth::id();
         $topic->save();
         return redirect()->route('topics.show', $topic->id)->with('info', '成功创建话题！');
     }
 
-    public function edit(Topic $topic) {
+    public function edit(Topic $topic)
+    {
         $this->authorize('update', $topic);
-        return view('topics.create_and_edit', compact('topic'));
+        $categories = Category::all();
+        return view('topics.create_and_edit', compact('topic', 'categories'));
     }
 
-    public function update(TopicRequest $request, Topic $topic) {
+    public function update(TopicRequest $request, Topic $topic)
+    {
         $this->authorize('update', $topic);
         $topic->update($request->all());
 
         return redirect()->route('topics.show', $topic->id)->with('info', '更新成功！');
     }
 
-    public function destroy(Topic $topic) {
+    public function destroy(Topic $topic)
+    {
         $this->authorize('destroy', $topic);
         $topic->delete();
-
         return redirect()->route('topics.index')->with('info', '成功删除！');
     }
-    
+
     public function uploadImage(Request $request, ImageUploadHandler $uploader)
     {
         // 初始化返回数据，默认是失败的
         $data = [
-            'success'   => false,
-            'msg'       => '上传失败!',
+            'success' => false,
+            'msg' => '上传失败!',
             'file_path' => ''
         ];
         // 判断是否有上传文件，并赋值给 $file
@@ -71,10 +80,11 @@ class TopicController extends Controller {
             // 图片保存成功的话
             if ($result) {
                 $data['file_path'] = $result['path'];
-                $data['msg']       = "上传成功!";
-                $data['success']   = true;
+                $data['msg'] = "上传成功!";
+                $data['success'] = true;
             }
         }
         return $data;
     }
+
 }
