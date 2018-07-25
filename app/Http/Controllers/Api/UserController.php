636a1b9736b737b7dd4876e0aa9d83fcf739c0c4
Xiyoulan <740518003@qq.com>
2018-07-25 10:28:30 +0000

    api/user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Transformers\UserTransformer;
 use App\Http\Requests\Api\UserRequest;
 
 class UserController extends Controller
@@ -20,14 +21,23 @@ class UserController extends Controller
             // 返回401
             return $this->response->errorUnauthorized('验证码错误');
         }
-        User::create(['name' => $request->name,
-            'phone' => $verifyData['phone'],
-            'password' => bcrypt($request->password),
+        $user = User::create(['name' => $request->name,
+                    'phone' => $verifyData['phone'],
+                    'password' => bcrypt($request->password),
         ]);
         //清除验证码缓存
         \Cache::forget($request->verification_key);
         //201
-        return $this->response()->created();
+        return $this->response->item($user, new UserTransformer())->setMeta([
+                            'access_token' => \Auth::guard('api')->fromUser($user),
+                            'token_type' => 'Bearer',
+                            'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+                        ])->setStatusCode(201);
+    }
+
+    public function me()
+    {
+        return $this->response->item($this->user(), new UserTransformer());
     }
 
 }
